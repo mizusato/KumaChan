cd92d521c1d53f02ff30e209e7c41be30e87c710
mizusato <mizusato@protonmail.com>
2019-02-18 19:21:48 +0800

    backup

---
+++ runtime/go/kumago.go
@@ -0,0 +1,274 @@
+package main
+
+
+import "fmt"
+
+
+type Type int
+
+
+const (
+    Integer Type = iota
+    Number
+    Bool
+    String
+    List
+)
+
+
+type Object interface {
+    get_type() Type
+}
+
+
+type IntegerObject int64
+type NumberObject float64
+type BoolObject bool
+type StringObject string
+
+
+func (n IntegerObject) get_type() Type { return Integer }
+func (x NumberObject) get_type() Type { return Number }
+func (t BoolObject) get_type() Type { return Bool }
+func (s StringObject) get_type() Type { return String }
+
+
+const LIST_INIT_SIZE = 30  // even integer
+const LIST_GROW = 10  // grow factor
+const LIST_WASTE_BOUND = 8  // ratio to 12 (0 < integer < 12)
+
+
+type LinearList struct {
+    data []Object
+    head int
+    tail int
+    center int
+}
+
+
+func make_list() LinearList {
+    return LinearList {
+        data: make([]Object, LIST_INIT_SIZE*2+1),
+        head: LIST_INIT_SIZE,
+        tail: LIST_INIT_SIZE,
+        center: LIST_INIT_SIZE,
+    }
+}
+
+
+func (l *LinearList) length() int {
+    return (l.tail - l.head)
+}
+
+
+func (l *LinearList) at(index int) Object {
+    if 0 <= index && index < (l.tail - l.head) {
+        value := l.data[l.head + index]
+        if value != nil {
+            return value
+        } else {
+            panic("nil element in linear list")
+        }
+    } else {
+        panic("linear list index error")
+    }
+}
+
+
+func (l *LinearList) prepend (element Object) {
+    capacity := len(l.data)
+    if (l.head == 0) {
+        offset_backward := l.center
+        grow_backward := offset_backward * LIST_GROW
+        delta := grow_backward - offset_backward
+        new_capacity := capacity + delta
+        new_data := make([]Object, new_capacity)
+        for i := l.head; i < l.tail; i++ {
+            new_data[i+delta] = l.data[i]
+        }
+        l.data = new_data
+        l.center += delta
+        l.head += delta
+        l.tail += delta
+        fmt.Printf("grow: %v -> %v\n", capacity, new_capacity)
+    }
+    if (l.head != l.tail) {
+        l.head -= 1
+    }
+    l.data[l.head] = element
+}
+
+
+func (l *LinearList) shift () {
+    if (l.head != l.tail) {
+        l.data[l.head] = nil
+        l.head += 1
+        l.check_waste()
+    } else {
+        panic("linear list has no element to shift")
+    }
+}
+
+
+func (l *LinearList) append(element Object) {
+    capacity := len(l.data)
+    if (l.tail == capacity) {
+        offset_forward := (capacity - l.center) - 1
+        grow_forward := offset_forward * LIST_GROW
+        new_capacity := capacity + (grow_forward - offset_forward)
+        new_data := make([]Object, new_capacity)
+        for i := l.head; i < l.tail; i++ {
+            new_data[i] = l.data[i]
+        }
+        l.data = new_data
+        fmt.Printf("grow: %v -> %v\n", capacity, new_capacity)
+    }
+    l.data[l.tail] = element
+    l.tail += 1
+}
+
+
+func (l *LinearList) pop() {
+    if (l.head != l.tail) {
+        l.tail -= 1
+        l.data[l.tail] = nil
+        l.check_waste()
+    } else {
+        panic("linear list has no element to pop")
+    }
+}
+
+
+func (l *LinearList) insert_left(position int, element Object) {
+    length := l.tail - l.head
+    if 0 <= position && position < length {
+        if (length == 0) {
+            l.append(element)
+        } else {
+            var last_element = l.data[l.tail-1]
+            for i := l.tail-1; i > l.head + position; i-- {
+                l.data[i] = l.data[i-1]
+            }
+            l.data[l.head + position] = element
+            l.append(last_element)
+        }
+    } else {
+        panic("linear list index error")
+    }
+}
+
+
+func (l *LinearList) insert_right(position int, element Object) {
+    length := l.tail - l.head
+    if 0 <= position && position < length {
+        if (length == 0) {
+            l.prepend(element)
+        } else {
+            var first_element = l.data[l.head]
+            for i := l.head; i < l.head + position; i++ {
+                l.data[i] = l.data[i+1]
+            }
+            l.data[l.head + position] = element
+            l.prepend(first_element)
+        }
+    } else {
+        panic("linear list index error")
+    }
+}
+
+
+func (l *LinearList) check_waste() {
+    capacity := len(l.data)
+    length := l.tail - l.head
+    if length == 0 {
+        /* if the last element has been removed, move head to center */
+        l.head = l.center
+        l.tail = l.head
+    } else {
+        /* if head or tail ran far away from center, pull data back */
+        run_forward := l.head - l.center
+        run_backward := l.center - (l.tail-1)
+        bound_forward := (capacity - l.center - 1) * LIST_WASTE_BOUND / 12
+        bound_backward := (l.center) * LIST_WASTE_BOUND / 12
+        if run_forward > bound_forward && run_forward >= length {
+            new_head := l.center
+            for i := 0; i < length; i++ {
+                old_index := l.head + i
+                l.data[new_head + i] = l.data[old_index]
+                l.data[old_index] = nil
+            }
+            l.head = new_head
+            l.tail = l.head + length
+        } else if run_backward > bound_backward && run_backward >= length {
+            new_head := (l.center+1) - length
+            for i := 0; i < length; i++ {
+                old_index := l.head + i
+                l.data[new_head + i] = l.data[old_index]
+                l.data[old_index] = nil
+            }
+            l.head = new_head
+            l.tail = l.head + length
+        }
+    }
+}
+
+
+func main() {
+    /*
+    n := Object(IntegerObject(3))
+    x := Object(NumberObject(1.2) + NumberObject(2.4))
+    fmt.Println(n)
+    fmt.Println(x)
+    */
+    /*
+    l := make_list()
+    for i:=0; i<1000; i++ {
+        l.append(IntegerObject(i))
+    }
+    fmt.Println(l.length())
+    for i:=0; i<100; i++ {
+        l.pop()
+    }
+    fmt.Println(l.length())
+    for i:=0; i<1000; i++ {
+        l.prepend(IntegerObject(i))
+    }
+    fmt.Println(l.length())
+    for i:=0; i<500; i++ {
+        l.shift()
+    }
+    fmt.Println(l.length())
+    for i:=0; i < l.length(); i++ {
+        fmt.Printf("%v, ", l.at(i))
+    }
+    */
+    /*
+    l := make_list()
+    l.append(IntegerObject(-6))
+    l.append(IntegerObject(-5))
+    l.append(IntegerObject(-4))
+    l.append(IntegerObject(-3))
+    l.append(IntegerObject(-2))
+    l.append(IntegerObject(-1))
+    for i:=0; i < 10000; i++ {
+        l.prepend(IntegerObject(i))
+        l.pop()
+    }
+    */
+    l := make_list()
+    l.append(IntegerObject(0))
+    l.append(IntegerObject(1))
+    l.append(IntegerObject(2))
+    l.append(IntegerObject(3))
+    l.append(IntegerObject(4))
+    l.append(IntegerObject(5))
+    l.append(IntegerObject(6))
+    fmt.Println(l.length())
+    l.insert_left(3, NumberObject(355.0/113.0))
+    l.insert_right(4, StringObject("7/2"))
+    fmt.Println(l.length())
+    for i:=0; i < l.length(); i++ {
+        fmt.Printf("%v, ", l.at(i))
+    }
+    fmt.Println("")
+}
