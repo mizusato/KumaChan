3d23368137277beb9e73884824c17937369f6a66
mizusato <mizusato@protonmail.com>
2019-03-20 20:03:26 +0800

    refactor class and interface

--- refactor/runtime.js
+++ refactor/runtime.js
@@ -25,13 +25,17 @@
         arg_immutable: name => `immutable value for dirty argument ${name}`,
         retval_invalid: 'invalid return value',
         no_matching_function: 'invalid arguments: no matching function',
-        expose_non_instance: 'unable to expose non-instance object',
-        method_name_conflict: name => `conflict method name: ${name}`,
-        impl_not_exposing: C => `instance does not expose instance of ${C}`,
-        impl_not_matching: I => `instance does not implement ${I}`,
-        method_not_matching: name => (
-            `method ${name}() does not match required signature`
-        )
+        method_conflict: (A1, name, A2) => (
+            `exposed method conflict: ${A1} and ${A2} both have method ${name}`
+        ),
+        method_missing: (name, C, I) => (
+            `class ${C} does not implement ${I} (missing method ${name})`
+        ),
+        method_invalid: (name, C, I) => (
+            `class ${C} does not implement ${I} (invalid method ${name})`
+        ),  
+        exposing_non_instance: 'unable to expose non-instance object',
+        not_exposing: C => `created instance does not expose instance of ${C}`,
     }
 
     /**
@@ -44,6 +48,7 @@
     class AssignError extends RuntimeError {}
     class AccessError extends RuntimeError {}
     class CallError extends RuntimeError {}
+    class ClassError extends RuntimeError {}
     class InitError extends RuntimeError {}
     
     class ErrorProducer {
@@ -84,6 +89,10 @@
         return result
     }
 
+    function run (iterable) {
+        for (let I of iterable) {}
+    }
+
     function *rev (list) {
         for (let i=list.length-1; i>=0; i--) {
             yield list[i]
@@ -120,6 +129,14 @@
             yield f(key, object[key])
         }
     }
+
+    function foreach (something, f) {
+        if (typeof something[Symbol.iterator] == 'function') {
+            return run(map(something, f))
+        } else {
+            return run(mapkv(something, f))
+        }
+    }
     
     function *filter (iterable, f) {
         let index = 0
@@ -143,6 +160,14 @@
 
     function *cat (...iterables) {
         for (let iterable of iterables) {
+            for (let I of iterable) {
+                yield I
+            }
+        }
+    }
+
+    function *flat (iterable_of_iterable) {
+        for (let iterable of iterable_of_iterable) {
             for (let I of iterable) {
                 yield I
             }
@@ -985,39 +1010,131 @@
      *  Class & Instance
      */
 
-    let exp_err = new ErrorProducer(CallError, 'expose()')
+    let exp_err = new ErrorProducer(CallError, '::expose()')
 
     function add_exposed_internal(internal, instance) {
         // expose interface of internal object
         assert(!instance.init_finished)
-        exp_err.assert(internal instanceof Instance, MSG.expose_non_instance)
+        exp_err.assert(
+            internal instanceof Instance,
+            MSG.exposing_non_instance
+        )
         instance.exposed.push(internal)
-        for (let name of Object.keys(internal.methods)) {
-            exp_err.assert(
-                !has(name, instance.methods),
-                MSG.method_name_conflict(name)
-            )
-            instance.methods[name] = internal.methods[name]
-        }        
+        foreach(internal.methods, (name, method) => {
+            assert(!has(name, instance.methods))
+            instance.methods[name] = method
+        })
     }
 
-    function is_direct_instance_of (class_, instance) {
+    function class_error_tools (class_) {
+        let err = new ErrorProducer(ClassError, '::create_class()')
+        let msg_conflict = (info1, name, info2) => (
+            MSG.method_conflict(info1.from.desc, name, info2.from.desc)
+        )
+        let conflict_if = ((bool, i1, name, i2) => err.assert(
+            !bool, bool && msg_conflict(i1, name, i2)
+        ))
+        let msg_missing = (name, I) => (
+            MSG.method_missing(name, class_.desc, I.desc)
+        )
+        let missing_if = (bool, name, I) => err.assert(
+            !bool, bool && msg_missing(name, I)
+        )
+        let msg_invalid = (name, I) => (
+            MSG.method_invalid(name, class_.desc, I.desc)
+        )
+        let invalid_if = (bool, name, I) => err.assert(
+            !bool, bool && msg_invalid(name, I)
+        )
+        return { conflict_if, missing_if, invalid_if }
+    }
+    
+    let only_classes = (x => filter(x, y => y instanceof Class))
+    let only_interfaces = (x => filter(x, y => y instanceof Interface))
+
+    function get_methods_info (class_) {
         assert(class_ instanceof Class)
-        assert(instance instanceof Instance)
-        return (instance.abstraction === class_)
+        let { conflict_if, missing_if, invalid_if } = class_error_tools(class_)
+        // create empty info: { name -> { method, from: class or interface } }
+        let info = {}
+        // add own methods
+        foreach(class_.methods, (name, method) => {
+            info[name] = { method: method, from: class_ }
+        })
+        // add exposed methods (inherited methods)
+        foreach(only_classes(class_.impls), super_class => {
+            foreach(super_class.methods_info, (name, method_info) => {
+                conflict_if(has(name, info), info[name], name, method_info)
+                info[name] = { method: method_info.method, from: super_class }
+            })
+        })
+        foreach(only_interfaces(class_.impls), I => {
+            // add interface methods (default implementations)
+            foreach(I.defaults, (name, method) => {
+                if (!has(name, info)) {
+                    info[name] = { method: method, from: I }
+                } else {
+                    let is_default = (info[name].from instanceof Interface)
+                    conflict_if(is_default, info[name], name, { from: I })
+                }
+            })
+            // check if implement the interface I
+            foreach(I.sign_table, (name, signature) => {
+                missing_if(!has(name, info), name, I)
+                invalid_if(!is(info[name].method, signature), name, I)
+            })
+        })
+        // output the final info
+        return info
     }
 
-    function is_instance_of (class_, instance) {
-        assert(class_ instanceof Class)
-        assert(instance instanceof Instance)
-        if (is_direct_instance_of(class_, instance)) {
-            return true
-        } else {
-            return exists (
-                instance.exposed,
-                e => is_instance_of(class_, e)
+    function get_super_classes (class_) {
+        // get all [ S ∈ Class | C ⊂ S ] in which C is the argument class_
+        function *_get_super_classes (class_) {
+            return cat(
+                [class_], flat(map(
+                    only_classes(class_.impls), super_class => (
+                        _get_super_classes(super_class)
+                    )
+                ))
             )
         }
+        return list(_get_super_classes(class_))
+    }
+
+    function get_super_interfaces (class_) {
+        // get all [ I ∈ Interface | C ⊂ I ] in which C is the argument class_
+        return list(flat(map(
+            class_.impls,
+            I => (I instanceof Class)? I.super_interfaces: [I]
+        )))
+    }
+
+    function apply_defaults (interface_, instance) {
+        let defaults = interface_.defaults
+        if (defaults.length == 0) { return }
+        // create the context scope for default implementations
+        let interface_scope = new Scope(null)
+        // filter methods
+        let names = list(mapkv(interface_.sign_table, name => name))
+        let f_implemented = (name => has(name, instance.methods))
+        let f_not_implemented = (name => !has(name, instance.methods))
+        let implemented = list(filter(names, f_implemented))
+        let not_implemented = list(filter(names, f_not_implemented))
+        // add implemented methods to the context scope
+        foreach(implemented, name => {
+            interface_scope.declare(name, instance.methods[name])
+        })
+        // for each default implementation
+        foreach(not_implemented, name => {
+            assert(has(name, defaults))
+            // create a method
+            let method = bind_context(defaults[name], interface_scope)
+            // add to the context scope
+            interface_scope.declare(name, method)
+            // add to the instance
+            instance.methods[name] = method
+        })
     }
 
     let MethodTable = hash_of(Type.Function.Wrapped)
@@ -1034,6 +1151,9 @@
             this.init = cancel_binding(init)
             this.methods = Object.freeze(methods)
             this.desc = desc
+            this.methods_info = Object.freeze(get_methods_info(this))
+            this.super_classes = Object.freeze(get_super_classes(this))
+            this.super_interfaces = Object.freeze(get_super_interfaces(this))
             let F = init[WrapperInfo]
             let err = new ErrorProducer(InitError, desc)
             this.create = wrap(
@@ -1052,15 +1172,14 @@
                     for (let I of impls) {
                         if (I instanceof Class) {
                             err.assert(
-                                is_instance_of(I, self),
-                                MSG.impl_not_exposing(I.desc)
+                                exists(
+                                    self.exposed,
+                                    instance => (instance.abstraction === I)
+                                ),
+                                MSG.not_exposing(I.desc)
                             )
                         } else if (I instanceof Interface) {
-                            let result = I.check_and_apply_defaults(self)
-                            err.assert(
-                                result == 'OK',
-                                MSG.impl_not_matching(I.desc) + ': ' + result
-                            )
+                            apply_defaults(I, self)
                         }
                     }
                     self.init_finish()
@@ -1068,14 +1187,10 @@
                 }
             )
             this[Checker] = (object => {
-                if (object instanceof Instance) {
-                    return (
-                        object.abstraction === this
-                        || exists(object.abstraction.impls, I => I === this)
-                    )
-                } else {
-                    return false
-                }
+                return exists(
+                    object.abstraction.super_classes,
+                    super_class => super_class === this
+                )
             })
             Object.freeze(this)
         }
@@ -1094,9 +1209,9 @@
             this.scope = scope
             this.exposed = []
             this.methods = mapval(methods, f => bind_context(f, scope))
-            for (let name of Object.keys(this.methods)) {
-                this.scope.declare(name, this.methods[name])
-            }
+            foreach(this.methods, (name, method) => {
+                this.scope.declare(name, method)
+            })
             this.init_finished = false
         }
         init_finish () {
@@ -1160,58 +1275,23 @@
             assert(is(sign_table, SignTable))
             assert(is(defaults, MethodTable))
             assert(is(desc, Type.String))
+            assert(forall(Object.keys(defaults), name => (
+                has(name, sign_table) && is(defaults[name], sign_table[name])
+            )))
             this.sign_table = Object.freeze(sign_table)
             this.defaults = Object.freeze(defaults)
             this.desc = desc
             this[Checker] = (instance => {
                 if (instance instanceof Instance) {
-                    return exists(instance.abstraction.impls, x => x === this)
+                    return exists(
+                        instance.abstraction.super_interfaces,
+                        I => (I === this)
+                    )
                 } else {
                     return false
                 }
             })
             Object.freeze(this)
-        }
-        check_and_apply_defaults (instance) {
-            assert(instance instanceof Instance)
-            let has_defaults = (Object.keys(this.defaults).length > 0)
-            let interface_scope = has_defaults? new Scope(null): null
-            for (let name of Object.keys(this.sign_table)) {
-                let has_this = has(name, instance.methods)
-                let method = instance.methods[name] || null
-                let ok_existing = has_this && is(method, this.sign_table[name])
-                let ok_defaults = !has_this && has(name, this.defaults)
-                if (ok_existing || ok_defaults) {
-                    if (has_defaults) {
-                        interface_scope.declare(name, method)
-                    }
-                } else {
-                    return MSG.method_not_matching(name)
-                }
-            }
-            for (let name of Object.keys(this.defaults)) {
-                assert(has(name, this.sign_table))
-                if (!has(name, instance.methods)) {
-                    instance.methods[name] = bind_context(
-                        this.defaults[name], interface_scope
-                    )
-                }
-            }            
-            return 'OK'
-        }
-        check (instance) {
-            if (instance instanceof Instance) {
-                return forall(Object.keys(this.sign_table), name => (
-                    is(instance.methods[name], this.sign_table[name])
-                ))
-            } else {
-                return false
-            }
-        }
-        apply_defaults (instance) {
-            assert(!instance.init_finished)
-            let scope = new Scope(null)
-            
         }
         get [Symbol.toStringTag]() {
             return 'Interface'
