1a7f0646733a97dfd2df12439a9213aab3002e36
mizusato <mizusato@protonmail.com>
2019-05-21 21:15:45 +0800

    separate es and ES

--- runtime/built-in/es_compatible.js
+++ runtime/built-in/es_compatible.js
@@ -1,25 +1,29 @@
-let built_in_es_compatible = { es: {
-    Symbol: Types.ES_Symbol,
-    Object: Types.ES_Object,
-    Key: Types.ES_Key,
-    Class: Types.ES_Class,
-    Function: Types.ES_Function,
-    undefined: undefined,
-    null: null,
-    symbol: fun (
-        'function symbol (name: String) -> ES_Symbol',
-            name => Symbol(name)
-    ),
-    new: fun (
-        'function new (F: ES_Class) -> ES_Function',
-            F => {
-                let C = function constructor (...args) {
-                    return new (
-                        Function.prototype.bind
-                            .apply(F, [null, ...args])
-                    )()
+let built_in_es_compatible = {
+    ES: {
+        Symbol: Types.ES_Symbol,
+        Object: Types.ES_Object,
+        Key: Types.ES_Key,
+        Class: Types.ES_Class,
+        Function: Types.ES_Function,
+    },
+    es: {
+        undefined: undefined,
+        null: null,
+        symbol: fun (
+            'function symbol (name: String) -> ES_Symbol',
+                name => Symbol(name)
+        ),
+        new: fun (
+            'function new (F: ES_Class) -> ES_Function',
+                F => {
+                    let C = function constructor (...args) {
+                        return new (
+                            Function.prototype.bind
+                                .apply(F, [null, ...args])
+                        )()
+                    }
+                    return inject_desc(C, 'es_constructor')
                 }
-                return inject_desc(C, 'es_constructor')
-            }
-    ),
-}}
+        )
+    }
+}
