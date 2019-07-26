9f01586e7b149ad53d1c3dd1758535eea0a6ce2e
mizusato <mizusato@protonmail.com>
2019-07-26 16:35:21 +0800

    authentic fold() for observable

--- runtime/built-in/functions.js
+++ runtime/built-in/functions.js
@@ -365,12 +365,42 @@ let built_in_functions = {
     ),
     fold: f (
         'fold',
-        'function fold (i: Iterable, initial: Any, f: Arity<3>) -> Object',
-            (i, initial, f) => fold(iter(i), initial, (e, v, n) => {
+        'function fold (o: Observable, base: Any, f: Arity<3>) -> Observer',
+            (o, base, f) => observer(push => {
+                let v = base
+                let n = 0
+                return obsv(o).subscribe(subs({
+                    next: x => {
+                        v = call(f, [x, v, n])
+                        n += 1
+                    },
+                    error: e => push(e),
+                    complete: () => {
+                        push(v)
+                        push(Complete)
+                    }
+                }))
+            }),
+        'function fold (o: Observable, base: Any, f: Arity<2>) -> Observer',
+            (o, base, f) => observer(push => {
+                let v = base
+                return obsv(o).subscribe(subs({
+                    next: x => {
+                        v = call(f, [x, v])
+                    },
+                    error: e => push(e),
+                    complete: () => {
+                        push(v)
+                        push(Complete)
+                    }
+                }))
+            }),
+        'function fold (i: Iterable, base: Any, f: Arity<3>) -> Object',
+            (i, base, f) => fold(iter(i), base, (e, v, n) => {
                 return call(f, [e, v, n])
             }),
-        'function fold (i: Iterable, initial: Any, f: Arity<2>) -> Object',
-            (i, initial, f) => fold(iter(i), initial, (e,v) => {
+        'function fold (i: Iterable, base: Any, f: Arity<2>) -> Object',
+            (i, base, f) => fold(iter(i), base, (e,v) => {
                 return call(f, [e, v])
             })
     ),
