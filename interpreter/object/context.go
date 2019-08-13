420916e1ea86c3ca9ea06432a9b29ded9dc00604
mizusato <mizusato@protonmail.com>
2019-08-14 01:26:55 +0800

    fix

--- interpreter/object/context.go
+++ interpreter/object/context.go
@@ -5,7 +5,7 @@ type ObjectContext struct {
     __EnqueCallback     func(Object,[]Object)
 }
 
-func NewContext (enque_callback func(Object,[]Object)) *ObjectContext {
+func NewObjectContext (enque_callback func(Object,[]Object)) *ObjectContext {
     return &ObjectContext {
         __NextSingletonId: 4,
         __EnqueCallback: enque_callback,
--- interpreter/test.go
+++ interpreter/test.go
@@ -4,7 +4,7 @@ import "fmt"
 import "./object"
 
 func main () {
-    var obj_ctx = object.NewContext(nil)
+    var obj_ctx = object.NewObjectContext(nil)
     var s1 = object.NewSingleton(obj_ctx)
     var s2 = object.NewSingleton(obj_ctx)
     fmt.Printf("%+v\n%+v\n%+v\n", object.Nil, s1, s2)
