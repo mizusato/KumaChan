cd37a205a757a4e66082faef73757dd6fbb547c7
mizusato <mizusato@protonmail.com>
2019-08-23 21:30:32 +0800

    fix design

--- interpreter/object/category.go
+++ interpreter/object/category.go
@@ -1,22 +1,28 @@
 package object
 
-type ObjectCategory uint32
+type ObjectCategory int
 const (
-    OC_Singleton ObjectCategory = iota
+    OC_Singleton ObjectCategory = 0
+    // primitive
     OC_Bool
-    OC_Float
-    OC_Integer
-    OC_String
-    OC_Buffer
-    OC_List
-    OC_Hash
+    OC_Byte            // uint8
+    OC_Int             // int
+    OC_Float           // float64
+    OC_BigInt
+    OC_BigFloat
+    OC_String          // immutable
+    // reference
+    OC_Tuple           // immutable, consists of 2~4 elements
+    OC_Array           // type List = Array<Any>, type Bytes = Array<Byte>
+    OC_Map             // type Hash = Map<String, Any>
     OC_Function
     OC_Overload
-    OC_Iterator
-    OC_AsyncIterator
-    OC_Observer
-    OC_Promise
-    OC_Error
+    OC_Method
+    OC_Iterator        // Iterator<T>
+    OC_AsyncIterator   // AsyncIterator<T>
+    OC_Source          // Source<T>, just like Rx.Observable<T>
+    OC_Promise         // Promise<T>
+    OC_Error           // Error<T>, T = type of payload
     OC_Module
     OC_SubModule
     OC_Class
@@ -30,7 +36,6 @@ const (
     OC_NativeType
     OC_GenericType
     OC_SignatureType
-    OC_SubType
     OC_CompoundType
 )
 
--- interpreter/object/object.go
+++ interpreter/object/object.go
@@ -4,7 +4,6 @@ import "unsafe"
 
 type Object struct {
     __Category  ObjectCategory
-    __Inline32  uint32
-    __Inline64  uint64
+    __Inline    uint64
     __Pointer   unsafe.Pointer
 }
