5125fbecc159f8c914f243c48bebb3164fc1accc
mizusato <mizusato@protonmail.com>
2019-08-24 21:31:13 +0800

    type system plan

--- interpreter/object/category.go
+++ interpreter/object/category.go
@@ -11,32 +11,35 @@ const (
     OC_BigInt
     OC_BigFloat
     OC_String          // immutable
-    // reference
-    OC_Tuple           // immutable, consists of 2~4 elements
+    // container
     OC_Array           // type List = Array<Any>, type Bytes = Array<Byte>
     OC_Map             // type Hash = Map<String, Any>
-    OC_Function
-    OC_Overload
-    OC_Method
+    OC_ArrayView       // immutable reference of mutable array
+    OC_MapView         // immutable reference of mutable map
+    // pseudo container
     OC_Iterator        // Iterator<T>
-    OC_AsyncIterator   // AsyncIterator<T>
+    OC_Stream          // Stream<T>, async iterator
     OC_Source          // Source<T>, just like Rx.Observable<T>
     OC_Promise         // Promise<T>
-    OC_Error           // Error<T>, T = type of payload
+    // error
+    OC_Error           // Error<T> in which T = type of payload, immutable
+    // module
     OC_Module
-    OC_SubModule
-    OC_Class
-    OC_Instance
-    OC_Interface
-    OC_Schema
+    // function-like
+    OC_Function
+    OC_TypeTemplate    // { __Inflater: Object, __HasDefault: bool, ... }
+    // struct
+    OC_Schema          // callable(cast): SCHEMA(struct)
     OC_Struct
-    OC_Enum
-    OC_AnyType
-    OC_NeverType
-    OC_NativeType
-    OC_GenericType
+    // OO
+    OC_Class           // callable(construct): CLASS(arg1, ...)
+    OC_Interface
+    OC_Instance
+    // misc types
+    OC_OneElementType
+    OC_SimpleType      // consists of a func(Object)bool
     OC_SignatureType
-    OC_CompoundType
+    OC_CompoundType    // union of atomic/intersection types (may be a enum)
 )
 
 func (obj Object) Is (oc ObjectCategory) bool {
