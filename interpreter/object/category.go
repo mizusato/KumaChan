2cd814e4ee84a1758829ca48be2ff8417206f5b8
mizusato <mizusato@protonmail.com>
2019-08-26 19:13:02 +0800

    fix

--- interpreter/object/category.go
+++ interpreter/object/category.go
@@ -11,23 +11,8 @@ const (
     OC_BigInt
     OC_BigFloat
     OC_String          // immutable
-    // container
-    OC_Array           // type List = Array<Any>, type Bytes = Array<Byte>
-    OC_Map             // type Hash = Map<String, Any>
-    OC_ArrayView       // immutable reference of mutable array
-    OC_MapView         // immutable reference of mutable map
-    // pseudo container
-    OC_Iterator        // Iterator<T>
-    OC_Stream          // Stream<T>, async iterator
-    OC_Source          // Source<T>, just like Rx.Observable<T>
-    OC_Promise         // Promise<T>
-    // error
-    OC_Error           // Error<T> in which T = type of payload, immutable
-    // module
-    OC_Module
-    // function-like
-    OC_Function
-    OC_TypeTemplate    // { __Inflater: Object, __HasDefault: bool, ... }
+    // native
+    OC_NativeObject
     // struct
     OC_Schema          // callable(cast): SCHEMA(struct)
     OC_Struct
@@ -35,7 +20,10 @@ const (
     OC_Class           // callable(construct): CLASS(arg1, ...)
     OC_Interface
     OC_Instance
-    // misc types
+    // misc
+    OC_Module
+    OC_Function
+    OC_TypeTemplate    // { __Inflater: Object, __HasDefault: bool, ... }
     OC_OneElementType
     OC_SimpleType      // consists of a func(Object)bool
     OC_SignatureType
