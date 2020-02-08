6c08476e1bd20937c02364504074a1bd0fa95fdf
mizusato <mizusato@protonmail.com>
2020-02-09 00:02:06 +0800

    fix

--- native/types/types.go
+++ native/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+// TODO: remove this file: do not distinguish between native type representations
+
 type NativeTypeId uint
 const (
 	// Basic Types
@@ -11,10 +13,11 @@ const (
 	T_Int
 	// Collection Types
 	T_Bytes
-	T_Map
+	T_String
+	T_Array
 	T_Stack
-	T_Heap
-	T_List
+	T_Queue
+	T_Set
 	// Effect Type
 	T_Effect
 	// I/O Types
@@ -24,18 +27,19 @@ const (
 
 var __NativeTypes = map[string] NativeTypeId {
 	// Core
-	"Bit":  T_Bit,
-	"Byte":  T_Byte,
-	"Word":  T_Word,
-	"Dword": T_Dword,
-	"Qword": T_Qword,
-	"Int": T_Int,
-	"Bytes": T_Bytes,
-	"Map":   T_Map,
-	"Stack": T_Stack,
-	"Heap":  T_Heap,
-	"List": T_List,
-	"Effect": T_Effect,
+	"Bit":     T_Bit,
+	"Byte":    T_Byte,
+	"Word":    T_Word,
+	"Dword":   T_Dword,
+	"Qword":   T_Qword,
+	"Int":     T_Int,
+	"Bytes":   T_Bytes,
+	"String":  T_String,
+	"Array":   T_Array,
+	"Stack":   T_Stack,
+	"Queue":   T_Queue,
+	"Set":     T_Set,
+	"Effect":  T_Effect,
 	// I/O
 	"InputStream": T_InputStream,
 	"OutputStream": T_OutputStream,
