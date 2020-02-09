6a181715bb674b4c4de096fb479c41aaf8d34ff8
mizusato <mizusato@protonmail.com>
2020-02-09 16:59:12 +0800

    fix

--- runtime/lib/container/ltt/ltt.go
+++ runtime/lib/container/ltt/ltt.go
@@ -10,15 +10,31 @@ type LTT struct {
 	Value  Value
 	Left   *LTT
 	Right  *LTT
-	Dist   int
+	Dist   uint64
 }
 
-func Node(v Value) *LTT {
+func Node(v Value, left *LTT, right *LTT) *LTT {
+	return &LTT {
+		Value: v,
+		Left:  left,
+		Right: right,
+		Dist:  (1 + right.GetDist()),
+	}
+}
+func Leaf(v Value) *LTT {
 	return &LTT {
 		Value: v,
 		Left:  nil,
 		Right: nil,
 		Dist:  1,
+	}
+}
+
+func (node *LTT) GetDist() uint64 {
+	if node != nil {
+		return node.Dist
+	} else {
+		return 0
 	}
 }
 
@@ -37,31 +53,29 @@ func (node *LTT) Merge(another *LTT, cmp order.Compare) *LTT {
 	default:
 		panic("impossible branch")
 	}
-	var root_value = smaller.Value
-	var c1 = smaller.Left
-	var c2 = smaller.Right.Merge(bigger, cmp)
-	if c1.Dist >= c2.Dist {
-		return &LTT {
-			Value: root_value,
-			Left: c1,
-			Right: c2,
-			Dist: (c2.Dist + 1),
-		}
+	var v = smaller.Value
+	var a = smaller.Left
+	var b = smaller.Right.Merge(bigger, cmp)
+	if a.GetDist() >= b.GetDist() {
+		return Node(v, a, b)
 	} else {
-		return &LTT {
-			Value: root_value,
-			Left: c2,
-			Right: c1,
-			Dist: (c1.Dist + 1),
-		}
+		return Node(v, b, a)
 	}
 }
 
-func (node *LTT) Pop(cmp order.Compare) (Value, *LTT, bool) {
+func (node *LTT) Top() (Value, bool) {
+	if node != nil {
+		return node.Value, true
+	} else {
+		return nil, false
+	}
+}
+
+func (node *LTT) Popped(cmp order.Compare) (Value, *LTT, bool) {
 	var merged = node.Left.Merge(node.Right, cmp)
 	return node.Value, merged, (merged != nil)
 }
 
-func (node *LTT) Push(v Value, cmp order.Compare) *LTT {
-	return node.Merge(Node(v), cmp)
+func (node *LTT) Pushed(v Value, cmp order.Compare) *LTT {
+	return node.Merge(Leaf(v), cmp)
 }
