231a5f8be4dea162ce1bf1ddcd4cb997d8b4a365
mizusato <mizusato@protonmail.com>
2020-09-28 21:16:35 +0800

    vdom svg

--- qt/qtbinding/webui/webui.js
+++ qt/qtbinding/webui/webui.js
@@ -22,8 +22,18 @@
  *  @property {Signal.<function(target:string, id:string, tag:string): void>} ReplaceNode
  */
 
+const SVGNS = 'http://www.w3.org/2000/svg'
+
 /** @type {Object.<string,HTMLElement>} */
 let ElementRegistry = {}
+function createElement(parent, tag) {
+    let parent_el = ElementRegistry[parent]
+    if (tag == 'svg' || parent_el instanceof SVGElement) {
+        return document.createElementNS(SVGNS, tag)
+    } else {
+        return document.createElement(tag)
+    }
+}
 
 console.log('[WebUI] Script Loaded')
 window.addEventListener('load', _ => {
@@ -45,15 +55,15 @@ window.addEventListener('load', _ => {
         bridge.EmitEvent(handler, ev)
     }
     bridge.ApplyStyle.connect((id, key, val) => {
-        console.log('ApplyStyle', { id, key, val })
+        // console.log('ApplyStyle', { id, key, val })
         ElementRegistry[id].style[key] = val
     })
     bridge.EraseStyle.connect((id, key) => {
-        console.log('EraseStyle', { id, key })
+        // console.log('EraseStyle', { id, key })
         ElementRegistry[id].style[key] = ''
     })
     bridge.SetAttr.connect((id, name, val) => {
-        console.log('SetAttribute', { id, name, val })
+        // console.log('SetAttribute', { id, name, val })
         if (name == 'value') {
             ElementRegistry[id].value = val
         } else {
@@ -69,7 +79,7 @@ window.addEventListener('load', _ => {
         }
     })
     bridge.AttachEvent.connect((id, name, prevent, stop, handler) => {
-        console.log('AttachEvent', { id, name, prevent, stop, handler })
+        // console.log('AttachEvent', { id, name, prevent, stop, handler })
         let listener = create_listener(prevent, stop, handler)
         let el = ElementRegistry[id]
         el.addEventListener(name, listener)
@@ -77,7 +87,7 @@ window.addEventListener('load', _ => {
         el._events[name] = { listener, handler }
     })
     bridge.ModifyEvent.connect((id, name, prevent, stop) => {
-        console.log('ModifyEvent', { id, name, prevent, stop })
+        // console.log('ModifyEvent', { id, name, prevent, stop })
         let el = ElementRegistry[id]
         let old_event = el._events[name]
         let handler = old_event.handler
@@ -88,38 +98,38 @@ window.addEventListener('load', _ => {
         el._events[name] = { listener, handler }
     })
     bridge.DetachEvent.connect((id, name) => {
-        console.log('DetachEvent', { id, name })
+        // console.log('DetachEvent', { id, name })
         let el = ElementRegistry[id]
         let event = el._events[name]
         el.removeEventListener(name, event.listener)
         delete el._events[name]
     })
     bridge.SetText.connect((id, text) => {
-        console.log('SetText', { id, text })
+        // console.log('SetText', { id, text })
         ElementRegistry[id].textContent = text
     })
     bridge.AppendNode.connect((parent, id, tag) => {
-        console.log('AppendNode', { parent, id, tag })
-        let el = document.createElement(tag)
+        // console.log('AppendNode', { parent, id, tag })
+        let el = createElement(parent, tag)
         ElementRegistry[id] = el
         ElementRegistry[parent].appendChild(el)
     })
     bridge.RemoveNode.connect((parent, id) => {
-        console.log('RemoveNode', { parent, id })
+        // console.log('RemoveNode', { parent, id })
         ElementRegistry[parent].removeChild(ElementRegistry[id])
         delete ElementRegistry[id]
     })
     bridge.UpdateNode.connect((old_id, new_id) => {
-        console.log('UpdateNode', { old_id, new_id })
+        // console.log('UpdateNode', { old_id, new_id })
         let el = ElementRegistry[old_id]
         delete ElementRegistry[old_id]
         ElementRegistry[new_id] = el
     })
     bridge.ReplaceNode.connect((parent, old_id, new_id, tag) => {
-        console.log('ReplaceNode', { parent, old_id, new_id, tag })
+        // console.log('ReplaceNode', { parent, old_id, new_id, tag })
         let parent_el = ElementRegistry[parent]
         let old_el = ElementRegistry[old_id]
-        let new_el = document.createElement(tag)
+        let new_el = createElement(parent, tag)
         parent_el.insertBefore(new_el, old_el)
         parent_el.removeChild(old_el)
     })
