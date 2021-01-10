0d1ba55c9793924753475a2599324430b2c96afd
mizusato <mizusato@protonmail.com>
2021-01-10 22:32:36 +0900

    refactor directory structure (4)

--- test/library_test.go
+++ test/library_test.go
@@ -6,16 +6,16 @@ import (
 )
 
 
-const stdlib = "stdlib"
+const library = "library"
 
 func TestSeqCons(t *testing.T) {
-	var dir_path = getTestDirPath(t, stdlib)
+	var dir_path = getTestDirPath(t, library)
 	var mod_path = filepath.Join(dir_path, "container", "seq_cons.km")
 	expectStdIO(t, mod_path, "", "0\n1\n2\n")
 }
 
 func TestSeqOps(t *testing.T) {
-	var dir_path = getTestDirPath(t, stdlib)
+	var dir_path = getTestDirPath(t, library)
 	var mod_path = filepath.Join(dir_path, "container", "seq_ops.km")
 	expectStdIO(t, mod_path, "", "100\n")
 }
