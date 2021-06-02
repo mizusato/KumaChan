687435923868cdf942eddd9d8c64d6ba3f54bed2
mizusato <mizusato@protonmail.com>
2021-06-02 23:36:49 +0900

    fix

--- interpreter/runtime/vm2/frame/frame.go
+++ interpreter/runtime/vm2/frame/frame.go
@@ -11,6 +11,7 @@ type Frame struct {
 	argument  Value
 	data      AddrSpace
 }
+
 func CreateFrame(f UsualFuncValue, arg Value) *Frame {
 	const CacheLinePadLength = LocalSize(64 / unsafe.Sizeof(Value(nil)))
 	var req_size = f.Entity.Code.FrameRequiredSize()
@@ -22,42 +23,57 @@ func CreateFrame(f UsualFuncValue, arg Value) *Frame {
 		data:     data,
 	}
 }
+
 func (r *Frame) Func() UsualFuncValue {
 	return r.function
 }
+
 func (r *Frame) Arg() Value {
 	return r.argument
 }
+
 func (r *Frame) Code() *Code {
 	return &(r.function.Entity.Code)
 }
+
 func (r *Frame) Last() LocalAddr {
 	return LocalAddr(uint(len(r.data)) - 1)
 }
+
 func (r *Frame) Branch(f UsualFuncValue, arg Value) *Frame {
 	return &Frame {
-		function: f,
+		function: &ValFunc {
+			Entity:  f.Entity,
+			Context: r.function.Context,
+		},
 		argument: arg,
 		data:     r.data,
 	}
 }
+
 func (r *Frame) Static(addr LocalAddr) Value {
 	return r.function.Entity.Code.Static[addr]
 }
+
 func (r *Frame) Context(addr LocalAddr) Value {
 	return r.function.Context[addr]
 }
+
 func (r *Frame) Data(addr LocalAddr) Value {
 	return r.data[addr]
 }
+
 func (r *Frame) DataRange(addr LocalAddr, size LocalSize) ([] Value) {
 	var u = (addr + 1)
 	return r.data[u: (u + size)]
 }
+
 func (r *Frame) DataDstRef(instruction_index LocalAddr) *Value {
 	return &(r.data[(instruction_index + r.function.Entity.Code.Offset)])
 }
+
 func (r *Frame) WrapPanic(e interface{}) interface{} {
 	return e  // TODO
 }
+
 
